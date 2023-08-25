59738f808b2ede8392507e43521ebef1805e6e21
NEMEREM SAMUEL N <anselmsamuel1991@gmail.com>
2023-08-25 14:18:42 +0100

    lesson two

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+# 5 TYPES OF DATA TYPES
+
+*** Data types include
+1. Integer type (int)
+The int tyoe is used to represent whole numbers.
+  eg.  a = 10, int can also be represented in deximal binary, octal and hexadecimal forms.
+
+2. Float tyoe (float)
+The float is used to represent floating point values (numbers with decimal values). eg. f = 1.23
+
+3. Complex data tyoe
+Thus is helpful scientific and mathematics applications and electrical engineering applications. eg. a + bj
+
+4. Bool type
+The bool tyoe is used to represent boolean values. The only allowed values True and False which are internally represented in python as 0 and 1 respectively.
+
+5. String data type (str)
+It is the commonly used data tyoe in python. A string is a sequence of characters enclosed within single or double quotes. eg s = "samuel"
+***
