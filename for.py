bc0edbbfb2aa1f176228c5cc962d63dc69b23227
NEMEREM SAMUEL N <anselmsamuel1991@gmail.com>
2023-08-28 14:55:38 +0100

    for loop practice

---
+++ for.py
@@ -0,0 +1,41 @@
+# Creating a list of ten subjects and printing each of them in a list
+
+# Create three lists containing the following: 
+a = "first_name"           
+b = "Last_name"
+c = "full_name"
+
+# The first name of everyone in my class
+first_name = ["samuel", "chadwick", "miracle", "jackson", "princess"]
+
+# The second name of everyone in my class
+last_name = ["anselm", "royal", "chinaka", "josh", "justina"]
+
+# Combination of the first and last name
+full_name = input("first_name: ")
+last_name = input("last_name: ")
+sum = "first_name" + "last_name"
+
+for a in first_name:
+    print(a)
+for b in last_name:
+    print(b)
+for c in last_name:
+    print("samuel" + "anselm")
+    print("chadwick" + "royal")
+    print("miracle" + "chinaka")
+    print("jackson" + "josh")
+    print("princess" + "justina")
+
+
+
+
+
+
+
+
+
+
+
+
+
