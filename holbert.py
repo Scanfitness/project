51e83467862eebe99b0c2133887a88bbe8e44b91
NEMEREM SAMUEL N <anselmsamuel1991@gmail.com>
2023-09-01 17:33:09 +0100

    weekend assignment

---
+++ holbert.py
@@ -0,0 +1,43 @@
+# Write a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. Also print the integer stored in the variable number, followed by Battery street, followed by a new line.
+
+script = "Programming is like building a multilingual puzzle"
+print(script)
+
+number = 98
+print(f"{98}, Battery street")
+
+# Print the float stored in the variable number with a precision of 2 digits
+
+number = 3.14159
+print(f"Float: {number:.2f}")
+
+# Print 3 times a string stored in the variable str, followed by its first 9 characters.
+
+str = "Holberton School"
+print(f"{3 * str}{str[:9]}")
+
+# Print Welcome to Holberton School!
+
+str1 = "Holberton"
+str2 = "School"
+str3 = f"{str1} {str2}"
+print(f"{str3}!")
+
+# From the above print the following
+
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
+# Define Python
+
+definition = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(definition)
+
+
